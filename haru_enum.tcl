0fbbb0dac16401cbdedbf92bf337a9515dab0e5a
nico-robert <nicolas.robert.19000@gmail.com>
2023-05-24 12:31:52 +0200

    uses enum sequence instead of enum define (v1.2)

--- haru_enum.tcl
+++ haru_enum.tcl
@@ -1,180 +1,199 @@
-# Copyright (c) 2022 Nicolas ROBERT.
+# Copyright (c) 2022-2023 Nicolas ROBERT.
 # Distributed under MIT license. Please see LICENSE for details.
 # haru - Tcl binding for libharu (http://libharu.org/) PDF library.
 
-cffi::enum define HPdfPageLayout {
-    HPDF_PAGE_LAYOUT_SINGLE         0
-    HPDF_PAGE_LAYOUT_ONE_COLUMN     1
-    HPDF_PAGE_LAYOUT_TWO_CLUMN_LEFT 2
-    HPDF_PAGE_LAYOUT_TWO_CLUMN_RIGH 3
-    HPDF_PAGE_LAYOUT_EOF            4
+cffi::enum sequence HPdfPageLayout {
+    HPDF_PAGE_LAYOUT_SINGLE         
+    HPDF_PAGE_LAYOUT_ONE_COLUMN     
+    HPDF_PAGE_LAYOUT_TWO_CLUMN_LEFT 
+    HPDF_PAGE_LAYOUT_TWO_CLUMN_RIGH 
+    HPDF_PAGE_LAYOUT_EOF            
 }
 
-cffi::enum define HPdfPageMode {
-    HPDF_PAGE_MODE_USE_NONE     0
-    HPDF_PAGE_MODE_USE_OUTLINE  1
-    HPDF_PAGE_MODE_USE_THUMBS   2
-    HPDF_PAGE_MODE_FULL_SCREEN  3
-    HPDF_PAGE_MODE_EOF          4
+cffi::enum sequence HPdfPageMode {
+    HPDF_PAGE_MODE_USE_NONE     
+    HPDF_PAGE_MODE_USE_OUTLINE  
+    HPDF_PAGE_MODE_USE_THUMBS   
+    HPDF_PAGE_MODE_FULL_SCREEN  
+    HPDF_PAGE_MODE_EOF          
 }
 
-cffi::enum define HPdfPageSizes {
-    HPDF_PAGE_SIZE_LETTER    0
-    HPDF_PAGE_SIZE_LEGAL     1
-    HPDF_PAGE_SIZE_A3        2
-    HPDF_PAGE_SIZE_A4        3
-    HPDF_PAGE_SIZE_A5        4
-    HPDF_PAGE_SIZE_B4        5
-    HPDF_PAGE_SIZE_B5        6
-    HPDF_PAGE_SIZE_EXECUTIVE 7
-    HPDF_PAGE_SIZE_US4x6     8
-    HPDF_PAGE_SIZE_US4x8     9
-    HPDF_PAGE_SIZE_US5x7     10
-    HPDF_PAGE_SIZE_COMM10    11
-    HPDF_PAGE_SIZE_EOF       12
+cffi::enum sequence HPdfPageSizes {
+    HPDF_PAGE_SIZE_LETTER   
+    HPDF_PAGE_SIZE_LEGAL    
+    HPDF_PAGE_SIZE_A3       
+    HPDF_PAGE_SIZE_A4       
+    HPDF_PAGE_SIZE_A5       
+    HPDF_PAGE_SIZE_B4       
+    HPDF_PAGE_SIZE_B5       
+    HPDF_PAGE_SIZE_EXECUTIVE
+    HPDF_PAGE_SIZE_US4x6    
+    HPDF_PAGE_SIZE_US4x8    
+    HPDF_PAGE_SIZE_US5x7     
+    HPDF_PAGE_SIZE_COMM10    
+    HPDF_PAGE_SIZE_EOF       
 }
 
-cffi::enum define HPdfPageDirection {
-    HPDF_PAGE_PORTRAIT  0
-    HPDF_PAGE_LANDSCAPE 1
+cffi::enum sequence HPdfPageDirection {
+    HPDF_PAGE_PORTRAIT 
+    HPDF_PAGE_LANDSCAPE
 }
 
-cffi::enum define HPdfPageNumStyle {
-    HPDF_PAGE_NUM_STYLE_DECIMAL       0
-    HPDF_PAGE_NUM_STYLE_UPPER_ROMAN   1
-    HPDF_PAGE_NUM_STYLE_LOWER_ROMAN   2
-    HPDF_PAGE_NUM_STYLE_UPPER_LETTERS 3
-    HPDF_PAGE_NUM_STYLE_LOWER_LETTERS 4
-    HPDF_PAGE_NUM_STYLE_EOF           5
+cffi::enum sequence HPdfPageNumStyle {
+    HPDF_PAGE_NUM_STYLE_DECIMAL      
+    HPDF_PAGE_NUM_STYLE_UPPER_ROMAN  
+    HPDF_PAGE_NUM_STYLE_LOWER_ROMAN  
+    HPDF_PAGE_NUM_STYLE_UPPER_LETTERS
+    HPDF_PAGE_NUM_STYLE_LOWER_LETTERS
+    HPDF_PAGE_NUM_STYLE_EOF          
 }
 
-cffi::enum define HPdfWritingMode {
-    HPDF_WMODE_HORIZONTAL 0
-    HPDF_WMODE_VERTICAL   1
-    HPDF_WMODE_EOF        2
+cffi::enum sequence HPdfWritingMode {
+    HPDF_WMODE_HORIZONTAL
+    HPDF_WMODE_VERTICAL  
+    HPDF_WMODE_EOF       
 }
 
-cffi::enum define HPdfEncoderType {
-    HPDF_ENCODER_TYPE_SINGLE_BYTE   0
-    HPDF_ENCODER_TYPE_DOUBLE_BYTE   1
-    HPDF_ENCODER_TYPE_UNINITIALIZED 2
-    HPDF_ENCODER_UNKNOWN            3
+cffi::enum sequence HPdfEncoderType {
+    HPDF_ENCODER_TYPE_SINGLE_BYTE  
+    HPDF_ENCODER_TYPE_DOUBLE_BYTE  
+    HPDF_ENCODER_TYPE_UNINITIALIZED
+    HPDF_ENCODER_UNKNOWN           
 }
 
-cffi::enum define HPdfByteType {
-    HPDF_BYTE_TYPE_SINGLE  0
-    HPDF_BYTE_TYPE_LEAD    1
-    HPDF_BYTE_TYPE_TRIAL   2
-    HPDF_BYTE_TYPE_UNKNOWN 3
+cffi::enum sequence HPdfByteType {
+    HPDF_BYTE_TYPE_SINGLE 
+    HPDF_BYTE_TYPE_LEAD   
+    HPDF_BYTE_TYPE_TRIAL  
+    HPDF_BYTE_TYPE_UNKNOWN
 }
 
-cffi::enum define HPdfAnnotHighlightMode {
-    HPDF_ANNOT_NO_HIGHTLIGHT       0
-    HPDF_ANNOT_INVERT_BOX          1
-    HPDF_ANNOT_INVERT_BORDER       2
-    HPDF_ANNOT_DOWN_APPEARANCE     3
-    HPDF_ANNOT_HIGHTLIGHT_MODE_EOF 4
+cffi::enum sequence HPdfAnnotHighlightMode {
+    HPDF_ANNOT_NO_HIGHTLIGHT      
+    HPDF_ANNOT_INVERT_BOX         
+    HPDF_ANNOT_INVERT_BORDER      
+    HPDF_ANNOT_DOWN_APPEARANCE    
+    HPDF_ANNOT_HIGHTLIGHT_MODE_EOF
 }
-cffi::enum define HPdfAnnotIcon {
-    HPDF_ANNOT_ICON_COMMENT       0
-    HPDF_ANNOT_ICON_KEY           1
-    HPDF_ANNOT_ICON_NOTE          2
-    HPDF_ANNOT_ICON_HELP          3
-    HPDF_ANNOT_ICON_NEW_PARAGRAPH 4
-    HPDF_ANNOT_ICON_PARAGRAPH     5
-    HPDF_ANNOT_ICON_INSERT        6
-    HPDF_ANNOT_ICON_EOF           7
+
+cffi::enum sequence HPdfAnnotIcon {
+    HPDF_ANNOT_ICON_COMMENT      
+    HPDF_ANNOT_ICON_KEY          
+    HPDF_ANNOT_ICON_NOTE         
+    HPDF_ANNOT_ICON_HELP         
+    HPDF_ANNOT_ICON_NEW_PARAGRAPH
+    HPDF_ANNOT_ICON_PARAGRAPH    
+    HPDF_ANNOT_ICON_INSERT       
+    HPDF_ANNOT_ICON_EOF          
 }
-cffi::enum define HPdfColorSpace {
-    HPDF_CS_DEVICE_GRAY 0
-    HPDF_CS_DEVICE_RGB  1
-    HPDF_CS_DEVICE_CMYK 2
-    HPDF_CS_CAL_GRAY    3
-    HPDF_CS_CAL_RGB     4
-    HPDF_CS_LAB         5
-    HPDF_CS_ICC_BASED   6
-    HPDF_CS_SEPARATION  7
-    HPDF_CS_DEVICE_N    8
-    HPDF_CS_INDEXED     9
-    HPDF_CS_PATTERN     10
-    HPDF_CS_EOF         11
+
+cffi::enum sequence HPdfColorSpace {
+    HPDF_CS_DEVICE_GRAY
+    HPDF_CS_DEVICE_RGB 
+    HPDF_CS_DEVICE_CMYK
+    HPDF_CS_CAL_GRAY   
+    HPDF_CS_CAL_RGB    
+    HPDF_CS_LAB        
+    HPDF_CS_ICC_BASED  
+    HPDF_CS_SEPARATION 
+    HPDF_CS_DEVICE_N   
+    HPDF_CS_INDEXED    
+    HPDF_CS_PATTERN     
+    HPDF_CS_EOF         
 }
-cffi::enum define HPdfInfoType {
-    HPDF_INFO_CREATION_DATE 0
-    HPDF_INFO_MOD_DATE      1
-    HPDF_INFO_AUTHOR        2
-    HPDF_INFO_CREATOR       3
-    HPDF_INFO_PRODUCER      4
-    HPDF_INFO_TITLE         5
-    HPDF_INFO_SUBJECT       6
-    HPDF_INFO_KEYWORDS      7
-    HPDF_INFO_EOF           8
+cffi::enum sequence HPdfInfoType {
+    HPDF_INFO_CREATION_DATE
+    HPDF_INFO_MOD_DATE     
+    HPDF_INFO_AUTHOR       
+    HPDF_INFO_CREATOR      
+    HPDF_INFO_PRODUCER     
+    HPDF_INFO_TITLE        
+    HPDF_INFO_SUBJECT      
+    HPDF_INFO_KEYWORDS     
+    HPDF_INFO_EOF          
 }
-cffi::enum define HPdfEncryptMode {
-    HPDF_ENCRYPT_R2 2
-    HPDF_ENCRYPT_R3 3
+
+cffi::enum sequence HPdfEncryptMode {
+    HPDF_ENCRYPT_R2
+    HPDF_ENCRYPT_R3
 }
-cffi::enum define HPdfTextRenderingMode {
-    HPDF_FILL                 0
-    HPDF_STROKE               1
-    HPDF_FILL_THEN_STROKE     2
-    HPDF_INVISIBLE            3
-    HPDF_FILL_CLIPPING        4
-    HPDF_STROKE_CLIPPING      5
-    HPDF_FILL_STROKE_CLIPPING 6
-    HPDF_CLIPPING             7
-    HPDF_RENDERING_MODE_EOF   8
+
+cffi::enum sequence HPdfTextRenderingMode {
+    HPDF_FILL                
+    HPDF_STROKE              
+    HPDF_FILL_THEN_STROKE    
+    HPDF_INVISIBLE           
+    HPDF_FILL_CLIPPING       
+    HPDF_STROKE_CLIPPING     
+    HPDF_FILL_STROKE_CLIPPING
+    HPDF_CLIPPING            
+    HPDF_RENDERING_MODE_EOF  
 }
-cffi::enum define HPdfLineCap {
-    HPDF_BUTT_END              0
-    HPDF_ROUND_END             1
-    HPDF_PROJECTING_SCUARE_END 2
-    HPDF_LINECAP_EOF           3
+
+cffi::enum sequence HPdfLineCap {
+    HPDF_BUTT_END             
+    HPDF_ROUND_END            
+    HPDF_PROJECTING_SCUARE_END
+    HPDF_LINECAP_EOF          
 }
-cffi::enum define HPdfLineJoin {
-    HPDF_MITER_JOIN   0
-    HPDF_ROUND_JOIN   1
-    HPDF_BEVEL_JOIN   2
-    HPDF_LINEJOIN_EOF 3
+
+cffi::enum sequence HPdfLineJoin {
+    HPDF_MITER_JOIN  
+    HPDF_ROUND_JOIN  
+    HPDF_BEVEL_JOIN  
+    HPDF_LINEJOIN_EOF
 }
-cffi::enum define HPdfTextAlignment {
-    HPDF_TALIGN_LEFT    0
-    HPDF_TALIGN_RIGHT   1
-    HPDF_TALIGN_CENTER  2
-    HPDF_TALIGN_JUSTIFY 3
+
+cffi::enum sequence HPdfTextAlignment {
+    HPDF_TALIGN_LEFT   
+    HPDF_TALIGN_RIGHT  
+    HPDF_TALIGN_CENTER 
+    HPDF_TALIGN_JUSTIFY
 }
-cffi::enum define HPdfTransitionStyle {
-    HPDF_TS_WIPE_RIGHT                       0
-    HPDF_TS_WIPE_UP                          1
-    HPDF_TS_WIPE_LEFT                        2
-    HPDF_TS_WIPE_DOWN                        3
-    HPDF_TS_BARN_DOORS_HORIZONTAL_OUT        4
-    HPDF_TS_BARN_DOORS_HORIZONTAL_IN         5
-    HPDF_TS_BARN_DOORS_VERTICAL_OUT          6
-    HPDF_TS_BARN_DOORS_VERTICAL_IN           7
-    HPDF_TS_BOX_OUT                          8
-    HPDF_TS_BOX_IN                           9
-    HPDF_TS_BLINDS_HORIZONTAL                10
-    HPDF_TS_BLINDS_VERTICAL                  11
-    HPDF_TS_DISSOLVE                         12
-    HPDF_TS_GLITTER_RIGHT                    13
-    HPDF_TS_GLITTER_DOWN                     14
-    HPDF_TS_GLITTER_TOP_LEFT_TO_BOTTOM_RIGHT 15
-    HPDF_TS_REPLACE                          16
-    HPDF_TS_EOF                              17
+
+cffi::enum sequence HPdfTransitionStyle {
+    HPDF_TS_WIPE_RIGHT                     
+    HPDF_TS_WIPE_UP                        
+    HPDF_TS_WIPE_LEFT                      
+    HPDF_TS_WIPE_DOWN                      
+    HPDF_TS_BARN_DOORS_HORIZONTAL_OUT      
+    HPDF_TS_BARN_DOORS_HORIZONTAL_IN       
+    HPDF_TS_BARN_DOORS_VERTICAL_OUT        
+    HPDF_TS_BARN_DOORS_VERTICAL_IN         
+    HPDF_TS_BOX_OUT                        
+    HPDF_TS_BOX_IN                         
+    HPDF_TS_BLINDS_HORIZONTAL               
+    HPDF_TS_BLINDS_VERTICAL                 
+    HPDF_TS_DISSOLVE                        
+    HPDF_TS_GLITTER_RIGHT                   
+    HPDF_TS_GLITTER_DOWN                    
+    HPDF_TS_GLITTER_TOP_LEFT_TO_BOTTOM_RIGHT
+    HPDF_TS_REPLACE                         
+    HPDF_TS_EOF                             
 }
-cffi::enum define HPdfBlendMode {
-    HPDF_BM_NORMAL      0
-    HPDF_BM_MULTIPLY    1
-    HPDF_BM_SCREEN      2
-    HPDF_BM_OVERLAY     3
-    HPDF_BM_DARKEN      4
-    HPDF_BM_LIGHTEN     5
-    HPDF_BM_COLOR_DODGE 6
-    HPDF_BM_COLOR_BUM   7
-    HPDF_BM_HARD_LIGHT  8
-    HPDF_BM_SOFT_LIGHT  9
-    HPDF_BM_DIFFERENCE  10
-    HPDF_BM_EXCLUSHON   11
-    HPDF_BM_EOF         12
+
+cffi::enum sequence HPdfBlendMode {
+    HPDF_BM_NORMAL     
+    HPDF_BM_MULTIPLY   
+    HPDF_BM_SCREEN     
+    HPDF_BM_OVERLAY    
+    HPDF_BM_DARKEN     
+    HPDF_BM_LIGHTEN    
+    HPDF_BM_COLOR_DODGE
+    HPDF_BM_COLOR_BUM  
+    HPDF_BM_HARD_LIGHT 
+    HPDF_BM_SOFT_LIGHT 
+    HPDF_BM_DIFFERENCE  
+    HPDF_BM_EXCLUSHON   
+    HPDF_BM_EOF         
+}
+
+cffi::enum define HPdfShadingType {
+    HPDF_SHADING_FREE_FORM_TRIANGLE_MESH 4
+}
+
+cffi::enum sequence HPdfShading_FreeFormTriangleMeshEdgeFlag {
+    HPDF_FREE_FORM_TRI_MESH_EDGEFLAG_NO_CONNECTION     
+    HPDF_FREE_FORM_TRI_MESH_EDGEFLAG_BC   
+    HPDF_FREE_FORM_TRI_MESH_EDGEFLAG_AC        
 }
